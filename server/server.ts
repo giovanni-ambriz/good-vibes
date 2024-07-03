ab5bfca843e2a42eda0e886f05772d02b776f902
Hope Robertson <hope.robertson.nz@gmail.com>
2024-07-03 18:22:46 +1200

    started work on App.tsx, needs to be looked at, put some code in affirmationRoutes.ts, started modifying server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,32 @@
 import * as Path from 'node:path'
 import express from 'express'
+import affRouter from './routes/affirmationRoutes'
+import { Request, Response } from 'express'
 
 const server = express()
 
 server.use(express.json())
+
+// Mount affirmation route
+server.use('/affirmation', affRouter)
+
+server.get('/', async (req: Request, res: Response) => {
+  try {
+    const affirmation = await getAffirmation()
+    res.json({ affirmation })
+  } catch (error) {
+    console.error('Error fetching affirmation:', error)
+    res.status(500).json({ message: 'Error fetching affirmation' })
+  }
+})
+async function getAffirmation() {
+  try {
+    const response = await request.get('https;//www.affirmations.dev/')
+    return response.body
+  } catch (error) {
+    console.error('Error fetching affirmation:', error)
+    return 'Error fetching affirmation'
+  }
+}
 
 export default server
