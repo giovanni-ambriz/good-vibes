218bb07d1da323b4b9dc7bda34ba65a79bff7a20
Hope Robertson <hope.robertson.nz@gmail.com>
2024-07-03 16:44:26 +1200

    added pseudocode in routes files

---
+++ server/routes/affirmationRoutes.ts
@@ -0,0 +1,14 @@
+// imports go here
+
+// code for fetching data from affirmations.dev goes in this file
+
+// this API is cors-restricted
+
+// server.get('/api/v1/greeting', (req, res) => {
+//   const greetings = ['hola', 'hi', 'hello', 'howdy']
+//   const index = Math.floor(Math.random() * greetings.length)
+//   console.log(index)
+//   res.json({ greeting: greetings[index] })
+// })
+
+// server.use etc
---
+++ server/routes/imageRoutes.ts
@@ -0,0 +1,14 @@
+// imports go here
+
+// code for fetching images from unsplash API goes here
+
+// this API is not CORS restricted
+
+// server.get('/api/v1/greeting', (req, res) => {
+//   const greetings = ['hola', 'hi', 'hello', 'howdy']
+//   const index = Math.floor(Math.random() * greetings.length)
+//   console.log(index)
+//   res.json({ greeting: greetings[index] })
+// })
+
+// server.use etc
