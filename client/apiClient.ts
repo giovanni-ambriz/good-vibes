aa95cf63784fa3c156db9f9666ed3ffd04fba2cb
Giovanni Ambriz <giovanni.ambriz@hotmail.com>
2024-07-04 00:19:22 +1200

    Create functions to display affirmation and images

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,6 +1,17 @@
 import request from 'superagent'
+import { Affirmation } from '../../models/affirmations';
 
 // export async function getGreeting(): Promise<string> {
 //  const res = await request.get('/api/v1/greeting')
 //  return res.body.greeting
 // }
+
+export async function getAffirmation(): Promise<Affirmation> {
+  const res = await request.get('/api/v1/affirmations');
+  return res.body as Affirmation;
+}
+
+export async function getImages(): Promise<string[]> {
+    const response = await request.get('https://api.unsplash.com') //modify path with api key and endpoint 
+    return response.body.map((image: any) => image.urls.regular)
+}
--- client/components/AffDisplay.tsx
+++ client/components/AffDisplay.tsx
@@ -1,1 +1,29 @@
-// this is where functionality to display an affirmation should go
+import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { getAffirmation } from '../apiClient';
+
+export function useAffirmation() {
+  return useQuery<string, Error>({
+    queryKey: ['affirmation'],
+    queryFn: getAffirmation
+  })
+}
+
+export default function DisplayAffirmation() {
+  const { data, isLoading, isError } = useAffirmation()
+
+  if (isLoading) {
+    return <p>Loading affirmation...</p> // We could use a loading spinner for this
+  }
+
+  if (isError) {
+    return <p>Something went wrong!</p>
+  }
+
+  return (
+    <div>
+      <h2>Affirmation:</h2>
+      <p>{data.affirmation}</p>
+    </div>
+  )
+}
