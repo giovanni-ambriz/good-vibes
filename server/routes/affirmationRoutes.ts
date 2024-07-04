ad18a254ab051aec9b3c9e8eae9f0b6e0797acde
Lei Zheng <zhenglei530@gmail.com>
2024-07-04 12:37:52 +1200

    fix http porxy issue

--- server/routes/affirmationRoutes.ts
+++ server/routes/affirmationRoutes.ts
@@ -8,12 +8,13 @@ import request from 'superagent'
 
 const affRouter = express.Router()
 
-affRouter.get('/affirmations', async (req, res) => {
+affRouter.get('/', async (req, res) => {
   try {
     const response = await request.get('https://www.affirmations.dev/')
     // Handle successful response (extract affirmation data)
     const affirmation = response.body // Assuming affirmation data is in the response body
-    res.json({ affirmation })
+    console.log(affirmation)
+    res.json(affirmation)
   } catch (error) {
     console.error('Error fetching affirmation:', error)
     res.status(500).json({ message: 'Error fetching affirmation' })
