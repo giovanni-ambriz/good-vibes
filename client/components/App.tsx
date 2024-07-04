1537e07cb921f3ca4d7138e2216852d1ce0bfb93
Lei Zheng <zhenglei530@gmail.com>
2024-07-04 22:56:17 +1200

    check out code

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import DisplayAffirmation from './AffDisplay'
+import DisplayImages from './ImgDisplay'
 
 const App = () => {
   const [view, setView] = useState<'home' | 'affirmations'>('home')
@@ -14,36 +15,22 @@ const App = () => {
     ></div>
   )
 }
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Affirmations and Images Generator</h1>
+//       <h3>
+//         Click Affirm! and get a genuine affirmation and calm picture to remind
+//         you how great you are.
+//       </h3>
 
-/*
-
-const getGreeting = async () => {
-  const response = await fetch('/api/v1/affirmations')
-  if (!response.ok) {
-    throw new Error('Bad network response')
-  }
-  const data = await response.json()
-  return data.affirmation
-}
-
-const App = () => {
-  return (
-    <>
-      <h1>Affirmations and Images Generator</h1>
-      <h3>
-        Click Affirm! and get a genuine affirmation and calm picture to remind
-        you how great you are.
-      </h3>
-
-      <div>
-        <DisplayAffirmation />
-      </div>
-      <div>
-        <ImgDisplay />
-      </div>
-    </>
-  )
-}
+//       <div>
+//         <DisplayAffirmation />
+//       </div>
+//       <div>
+//         <DisplayImages />
+//       </div>
+//     </>
+//   )
+// }
 export default App
-
-*/
